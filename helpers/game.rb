c630759e6f37739318eec02d86578222784ae489
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2025-01-07 22:47:04 +0000

    Add paths to other files

--- helpers/game.rb
+++ helpers/game.rb
@@ -1,2 +1,7 @@
 #General gameplay
 #Start > Choose Maker or Guesser > Create code > Store code > Guess > Feedback on code > End Game
+
+require_relative 'config/game_config'
+require_relative 'function/generate_code'
+require_relative 'function/player_input'
+require_relative 'function/announce_results'
