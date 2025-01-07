efe8a60b9174fc8086d9454097627d81390b5a34
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2025-01-07 22:49:56 +0000

    Add basic gameflow as structure

--- helpers/game.rb
+++ helpers/game.rb
@@ -5,3 +5,13 @@ require_relative 'config/game_config'
 require_relative 'function/generate_code'
 require_relative 'function/player_input'
 require_relative 'function/announce_results'
+
+class Game
+    def initialize
+        #initialize variables
+    end
+
+    def start
+        #game flow
+    end
+end
