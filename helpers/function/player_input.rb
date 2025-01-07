931b29d66f74c026ba908f7cb199a648c50d4055
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2025-01-07 22:00:26 +0000

    Restructured valid_input? to use conditions

--- helpers/function/player_input.rb
+++ helpers/function/player_input.rb
@@ -6,19 +6,21 @@ module PlayerInput
         puts "Colors: #{GameConfig::VALID_COLORS.join(', ')}"
         input = gets.chomp.downcase.split
 
-        valid_input?(input)
+        valid_input?(input) ? input : get
     end
 
     def self.valid_input?(input)
-        invalid_colors = input.reject { |color| GameConfig::VALID_COLORS.include?(color) }
-
-        case invalid_input
-        when input.length != GameConfig::CODE_LENGTH
+        if input.length != GameConfig::CODE_LENGTH
             puts "Error: Please enter #{GameConfig::CODE_LENGTH} colors to continue."
-        when invalid_colors.any?
+            return false
+
+        invalid_colors = input.reject { |color| GameConfig::VALID_COLORS.include?(color) }
+        invalid_colors.any?
             puts "Error: Invalid colors. Please input a code from these colors: #{GameConfig::VALID_COLORS.join(', ')}."
+            return false
         else 
             puts 'Processing...'
+            return true
         end
     end
 end
