f6fdc90a490faaf090d428aa8102f5e45e11ff5c
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2025-01-07 22:44:48 +0000

    Add announce results module

--- helpers/function/announce_results.rb
+++ helpers/function/announce_results.rb
@@ -1,2 +1,12 @@
-#module
-#Holds conditions for whether guesser or chooser wins
+module AnnounceResults
+    def self.display(player_code, secret_code, max_guesses, guess_count)
+        case
+        when player_code == secret_code
+            puts 'The correct code has been guessed. Codebreaker wins!'
+        when guess_count >= max_guesses
+            puts "No more turns left. The code was: #{secret_code.join(', ')}. Codemaker wins!"
+        else
+            puts "Incorrect guess. #{max_guesses - guess_count} guesses left."
+        end
+    end
+end
