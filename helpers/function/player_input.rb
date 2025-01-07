7a63844161cfc67c1be30389c4fe3d4645d2b268
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2025-01-07 21:50:42 +0000

    Add valid_move? method

--- helpers/function/player_input.rb
+++ helpers/function/player_input.rb
@@ -5,5 +5,20 @@ module PlayerInput
         puts 'Enter code (example: green blue pink red)'
         puts "Colors: #{GameConfig::VALID_COLORS.join(', ')}"
         input = gets.chomp.downcase.split
+
+        valid_move?(input)
+    end
+
+    def self.valid_move?(input)
+        invalid_colors = input.reject { |color| GameConfig::VALID_COLORS.include?(color) }
+
+        case invalid_input
+        when input.length != GameConfig::CODE_LENGTH
+            puts "Error: Please enter #{GameConfig::CODE_LENGTH} colors to continue."
+        when invalid_colors.any?
+            puts "Error: Invalid colors. Please input a code from these colors: #{GameConfig::VALID_COLORS.join(', ')}."
+        else 
+            puts 'Processing...'
+        end
     end
 end
