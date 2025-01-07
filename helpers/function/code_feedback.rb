30e5aa09c9e5bf7dd29577a7ceb7defe1e563227
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2025-01-07 22:33:24 +0000

    Add code feedback module

--- helpers/function/code_feedback.rb
+++ helpers/function/code_feedback.rb
@@ -1,2 +1,17 @@
-#module 
-#Provides feedback on correctness of secret code
+module CodeFeedback
+    def self.call(guess, secret_code)
+        exact_matches = 0
+        color_matches = 0
+
+        guess.each_with_index do |color, index|
+            if color == secret_code[index]
+                exact_matches += 1
+            else if secret_code.include?(color) && secret_code[index] != color
+                color_matches += 1
+            end
+        end
+
+        puts "Red pegs: #{exact_matches}"
+        puts "White pegs: #{color_matches}"
+    end
+end
