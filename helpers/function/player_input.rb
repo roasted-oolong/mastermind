59b7fe365246fbce1742fc32fa8aafbc9a0325c9
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2025-01-08 20:02:25 +0000

    Add get_role method to PlayerInput module
    Part of the refactoring to allow player to make their own code

--- helpers/function/player_input.rb
+++ helpers/function/player_input.rb
@@ -24,4 +24,10 @@ module PlayerInput
         puts 'Processing...'
         return true
     end
+
+    def self.get_role
+        puts "Would you like to be the code breaker or maker?"
+        puts "Type 'breaker' or 'maker'"
+        player_role = gets.chomp.downcase
+    end
 end
