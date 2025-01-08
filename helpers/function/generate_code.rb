c8ec5785399262f29853aca3dadd0f202b86d03a
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2025-01-08 22:56:56 +0000

    Renamed argument in same_four and added comment

--- helpers/function/generate_code.rb
+++ helpers/function/generate_code.rb
@@ -3,7 +3,7 @@ module GenerateCode
     Array.new(code_length) { valid_colors.sample }
   end
 
-  def self.same_four(array_index)
-    Array.new(4, array_index[0])
+  def self.same_four(array) #generates four of the first value in a given array
+    Array.new(4, array[0])
   end
 end
