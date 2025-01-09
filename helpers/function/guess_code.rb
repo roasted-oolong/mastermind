e8e2fb6746fd39e264a7af5cff2feda9b5c1aebc
roasted-oolong <65867683+roasted-oolong@users.noreply.github.com>
2025-01-09 15:47:34 -0600

    Correct case switch variable

--- helpers/function/guess_code.rb
+++ helpers/function/guess_code.rb
@@ -2,7 +2,7 @@ require_relative '../config/game_config'
 
 module GuessCode
     def guess(guess, match_count_arr) #[number of exact matches, number of color matches]
-      case matching_colors
+      case guess
       when #exact matches + color matches == 0
         #continue to the next color in "VALID_COLORS"
         #return four of that same color
@@ -12,6 +12,5 @@ module GuessCode
       when #exact matches + color matches == 4
         #randomize elements of the array
       end
-      
     end
 end
