58c7cd3984afdf4f1ed48f47705c88307eef3df8
roasted-oolong <65867683+roasted-oolong@users.noreply.github.com>
2025-01-09 15:45:05 -0600

    Add pseudocode for guess method

--- helpers/function/guess_code.rb
+++ helpers/function/guess_code.rb
@@ -1,7 +1,17 @@
 require_relative '../config/game_config'
 
 module GuessCode
-    def guess(match_count_arr)
-      #Guess code...
+    def guess(match_count_arr) #[number of exact matches, number of color matches]
+      case matching_colors
+      when #exact matches + color matches == 0
+        #continue to the next color in "VALID_COLORS"
+        #return four of that same color
+      when #exact matches + color matches <=3 
+        #continue to the next color in "VALID_COLORS"
+        #return THREE of that same color in the last three places of the guess
+      when #exact matches + color matches == 4
+        #randomize elements of the array
+      end
+      
     end
 end
