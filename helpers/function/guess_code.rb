e99c83a962dfb476c80eacced308057ee14bbf7f
roasted-oolong <65867683+roasted-oolong@users.noreply.github.com>
2025-01-09 18:26:32 -0600

    Add real conditions and add path to generate_code

--- helpers/function/guess_code.rb
+++ helpers/function/guess_code.rb
@@ -1,15 +1,16 @@
 require_relative '../config/game_config'
+require_relative 'generate_code'
 
 module GuessCode
     def guess(guess, match_count) #number of exact matches + number of color matches
       case guess
-      when #exact matches + color matches == 0
+      when match_count == 0
         #continue to the next color in "VALID_COLORS"
         #return four of that same color
-      when #exact matches + color matches <=3 
+      when match_count <= 3
         #continue to the next color in "VALID_COLORS"
         #return THREE of that same color in the last three places of the guess
-      when #exact matches + color matches == 4
+      when match_count == 4
         #randomize elements of the array
       end
     end
