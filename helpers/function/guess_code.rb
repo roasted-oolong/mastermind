26b0755e71700943f2c9fd3db2b5a7d614e894bb
roasted-oolong <65867683+roasted-oolong@users.noreply.github.com>
2025-01-09 23:00:12 -0600

    Add ability for computer to guess code

--- helpers/function/guess_code.rb
+++ helpers/function/guess_code.rb
@@ -4,13 +4,17 @@ module GuessCode
     def guess(guess, match_count, valid_colors) #number of exact matches + number of color matches
       case guess
       when match_count == 0
-        #continue to the next color in "VALID_COLORS"
-        #return four of that same color
+        new_guess = guess.map { valid_colors[1] }
+
       when match_count <= 3
-        #continue to the next color in "VALID_COLORS"
-        #return THREE of that same color in the last three places of the guess
+        new_color_index = valid_colors.find_index(guess[match_count]) + 1
+        new_color = valid_colors[new_color_index]
+        new_guess = guess[0..match_count - 1] + Array.new(guess.length - match_count, new_color)
+        return new_guess
+
       when match_count == 4
-        #randomize elements of the array
+        new_guess = guess.shuffle
+        return new_guess
       end
     end
 end
