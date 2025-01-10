6dbf58bb63cc65213457f0651c77c5a203ec905c
roasted-oolong <65867683+roasted-oolong@users.noreply.github.com>
2025-01-10 00:36:20 -0600

    Fixed issue where switch case was not running correctly
    variable for case was incorrect

--- helpers/function/guess_code.rb
+++ helpers/function/guess_code.rb
@@ -2,17 +2,18 @@ require_relative 'generate_code'
 
 module GuessCode
     def self.guess(guess, match_count, valid_colors) #number of exact matches + number of color matches
-      case guess
-      when match_count == 0
+      case match_count
+      when 0
         new_guess = guess.map { valid_colors[1] }
+        return new_guess
 
-      when match_count <= 3
+      when 1..3
         new_color_index = valid_colors.find_index(guess[match_count]) + 1
         new_color = valid_colors[new_color_index]
         new_guess = guess[0..match_count - 1] + Array.new(guess.length - match_count, new_color)
         return new_guess
 
-      when match_count == 4
+      when 4
         new_guess = guess.shuffle
         return new_guess
       end
